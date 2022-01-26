f62941cd88cea18a9cad3ec56d1c21a92d85ea68
xilj <leon_xi@163.com>
2022-01-26 18:09:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ queryParam = {
     "tenantId": "0255b450ecf8139fcd3e0bc38584666d"
 }
 response = requests.post(queryUrl, queryParam)
-df = pd.read_json(StringIO(response.text), index=[0])
+df = pd.read_json(StringIO(response.text), dtype=object)
 
 arr = df['data']
 fig, ax = plt.subplots()
