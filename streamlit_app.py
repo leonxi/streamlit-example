b67617929005bdfa489876ad32ee76984862ce85
xilj <leon_xi@163.com>
2022-01-26 17:57:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import requests
 
 """
 # 使用 Streamlit! 构建 渠道来源分析报表
@@ -22,7 +23,17 @@ daterange = st.date_input("投递时间", (datetime.date(2022, 1, 1), datetime.d
 channel = st.selectbox("渠道", ["BOSS直聘", "51Job", "58同城"])
 position = st.selectbox("职位", ["Java后端软件工程师", "全栈软件工程师"])
 
-arr = np.random.randint(0,10,5)
+# 读取数据
+queryUrl = "http://116.63.135.62:8006/api/jasper/jasper/query/query"
+queryParam = {
+    filters: [],
+    reportId: "简历来源渠道分析",
+    tenantId: "0255b450ecf8139fcd3e0bc38584666d"
+}
+response = requests.post(queryUrl, queryParam)
+df = pd.read_json(StringIO(response.text))
+
+arr = df['data']
 fig, ax = plt.subplots()
 ax.pie(x = arr)
 
